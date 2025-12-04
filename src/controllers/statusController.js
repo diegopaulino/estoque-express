dd5cf9721be4e9ee2734a8cbbbbd4189cbdb9fb8
Diego <diegopaulino_3@hotmail.com>
2025-12-04 18:04:09 -0300

    feat: add 'routes'

---
+++ src/controllers/statusController.js
@@ -0,0 +1,42 @@
+import { MethodNotAllowedError } from "../infra/errors.js";
+import database from "../infra/database.js";
+
+const getStatus = async (req, res) => {
+  console.log("METODO USADO", req.method === "POST");
+  if (req.method === "POST") {
+    throw new MethodNotAllowedError();
+  }
+
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenConnectionsResult.rows[0].count;
+
+  res.status(200).send({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
+};
+
+export default getStatus;
